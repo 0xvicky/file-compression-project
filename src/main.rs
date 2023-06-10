ca9547ee1c46ae1420b3ab96baa6e9730a0c53ac
vicky <tyagivivek528@gmail.com>
2023-06-10 10:05:35 +0530

    Written code for encode the files using GzEncoder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("File compression project");
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main(){
+    if(args().len()!=3){
+        eprintln!("Usage `source` `target`");
+        return;
+    }
+    let mut input = BufReader::new(File.open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let encoder = GzEncoder::new(output, Compression::default());
+    
+    
 }
